00e30eae36a878b966c550ce063c4ad02b31f487
igarvaloshen <trapaholic.barisu4@gmail.com>
2016-09-07 00:45:26 +0300

    testimonials don't work from index

--- js/scriptMainPage.js
+++ js/scriptMainPage.js
@@ -2,8 +2,11 @@ $( function() {
     $(".j-testimonials").load("parts/testimonials.html", function() {
 
         <!-- scripts for testimonials block connect here start -->
-
+        $( "#tabs" ).tabs();
         <!-- scripts for testimonials block connect here end -->
 
     });
 } );
+
+// This will create a single gallery from all elements that have class "gallery-item"
+
