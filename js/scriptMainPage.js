b8fddae6e6a8c222d2bf9fa4405f4e3613437590
igarvaloshen <trapaholic.barisu4@gmail.com>
2016-09-11 20:54:00 +0300

    contacts complete

--- js/scriptMainPage.js
+++ js/scriptMainPage.js
@@ -7,6 +7,14 @@ $( function() {
 
     });
 } );
+$( function() {
+    $(".j-footer").load("parts/footer.html", function() {
+
+        <!-- scripts for testimonials block connect here start -->
+        <!-- scripts for testimonials block connect here end -->
+
+    });
+} );
 
 // This will create a single gallery from all elements that have class "gallery-item"
 
